4d79ee3e1cb711db491a96fe3534ea10292b830a
juanperezu <juanperez@elpoli.edu.co>
2021-10-26 14:55:57 -0500

    Con las clases de vida Saludable

---
+++ src/clases/Actividad.java
@@ -0,0 +1,26 @@
+package clases;
+
+public class Actividad {
+    private int cons;
+    private String nombre, descripcion;
+    public Actividad() {
+    }
+    public int getCons() {
+        return cons;
+    }
+    public void setCons(int cons) {
+        this.cons = cons;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getDescripcion() {
+        return descripcion;
+    }
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
---
+++ src/clases/Lugares.java
@@ -0,0 +1,28 @@
+package clases;
+
+public class Lugares {
+   private int cons;
+   private String direccion;
+   private String nombre ;
+public Lugares() {
+}
+public int getCons() {
+    return cons;
+}
+public void setCons(int cons) {
+    this.cons = cons;
+}
+public String getDireccion() {
+    return direccion;
+}
+public void setDireccion(String direccion) {
+    this.direccion = direccion;
+}
+public String getNombre() {
+    return nombre;
+}
+public void setNombre(String nombre) {
+    this.nombre = nombre;
+}
+   
+}
