def03869e7b921cb1c2c85295c41d0ad4a15e60b
juanperezu <juanperez@elpoli.edu.co>
2021-10-26 14:43:06 -0500

    mi proyeco

---
+++ src/clases/Restaurante.java
@@ -0,0 +1,50 @@
+public class Restaurante{
+private int cons, capacidad;
+private String nombre, direccion, pais, depto;
+private String localizacion;
+public Restaurante() {
+}
+public int getCons() {
+    return cons;
+}
+public void setCons(int cons) {
+    this.cons = cons;
+}
+public int getCapacidad() {
+    return capacidad;
+}
+public void setCapacidad(int capacidad) {
+    this.capacidad = capacidad;
+}
+public String getNombre() {
+    return nombre;
+}
+public void setNombre(String nombre) {
+    this.nombre = nombre;
+}
+public String getDireccion() {
+    return direccion;
+}
+public void setDireccion(String direccion) {
+    this.direccion = direccion;
+}
+public String getPais() {
+    return pais;
+}
+public void setPais(String pais) {
+    this.pais = pais;
+}
+public String getDepto() {
+    return depto;
+}
+public void setDepto(String depto) {
+    this.depto = depto;
+}
+public String getLocalizacion() {
+    return localizacion;
+}
+public void setLocalizacion(String localizacion) {
+    this.localizacion = localizacion;
+}
+
+}
